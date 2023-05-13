1107b9050d754cc52debac2ad5d22327d0e21ac5
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2023-05-13 06:52:53 -0700

    f
    f

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,18 +1,14 @@
-import me from '../app/placeholder.jpg';
+import me from "../app/placeholder.jpg";
 
-export const name = 'Your Name';
+export const name = "Your Name";
 export const avatar = me;
 export const about = () => {
-  return (
-    <>
-      Hey, I'm a developer who loves Next.js. I work at BigCo or CoolStartup as a Software Engineer.
-    </>
-  );
+  return <>Hey, I'm a developer who loves Next.js.</>;
 };
 export const bio = () => {
   return (
     <>
-      When I'm not coding, I'm probably playing video games or watching anime. I also love to travel and explore new places.
+      When I'm not coding, I'm probably playing video games or watching anime.
     </>
   );
 };
