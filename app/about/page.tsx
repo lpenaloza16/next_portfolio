4bbe07789aeb53bf5d6a55fefb26f9c388038666
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2024-01-02 20:05:34 -0800

    fillining

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,9 +2,25 @@ export default function AboutPage() {
   return (
     <section>
       <h1 className="font-bold text-3xl font-serif">About Me</h1>
-      <p className="my-5 text-neutral-800 dark:text-neutral-200">
-        This is my about page.
+      <p className="my-5 text-neutral-800 dark:text-neutral-200 leading-relaxed">
+        I'm Leo Pena, a Web Developer from California, passionate about crafting
+        efficient software solutions. My expertise spans JavaScript, Python,
+        React, and Java, with a current focus on mastering NextJS. I thrive in
+        collaborative environments, having contributed to Zero To Mastery and
+        various hackathons, and I'm committed to continuous learning, as
+        evidenced by my certifications and active engagement in coding
+        challenges. Beyond coding, my interests in Math and Science fuel my
+        creativity and analytical approach to problem-solving. I'm on the
+        lookout for web development and software engineering roles where I can
+        apply my skills, learn, and grow within an innovative team. Let's
+        connect and explore how we can collaborate on future tech projects!
       </p>
+      <a
+        href="https://www.linkedin.com/in/leonel-penaloza-78645a133/"
+        className="text-blue-600 hover:underline"
+      >
+        LinkedIn Profile
+      </a>
     </section>
   );
 }
